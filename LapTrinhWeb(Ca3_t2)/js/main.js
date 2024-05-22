85f15630e0f12efe02e7a551371b40cbf5ab92d3
Nhut Anh <thebestbook27@gmail.com>
2024-05-22 09:43:54 +0700

    nop

--- LapTrinhWeb(Ca3_t2)/js/main.js
+++ LapTrinhWeb(Ca3_t2)/js/main.js
@@ -1,9 +1,23 @@
-function hien(){
+function hiendn(){
     const a = document.getElementById("dn");
     a.style.display="flex";
     console.log("da bam")
 }
-function hien1(){
+function hiendn1(){
+    const b = document.getElementById("dk");
+    b.style.display="none"
+    const a = document.getElementById("dn");
+    a.style.display="flex";
+    console.log("da bam")
+}
+function hiendk(){
+    const a = document.getElementById("dk");
+    a.style.display="flex";
+    console.log("da bam")
+}
+function hiendk1(){
+    const b = document.getElementById("dn");
+    b.style.display="none";
     const a = document.getElementById("dk");
     a.style.display="flex";
     console.log("da bam")
